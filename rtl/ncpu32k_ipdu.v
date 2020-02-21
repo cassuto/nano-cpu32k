37c1d8a3195b685fe23487a266866a7adb1a5042
cassuto <diyer175@hotmail.com>
2020-02-21 19:48:41 +0800

    Updating ports of IPDU.

--- rtl/ncpu32k_ipdu.v
+++ rtl/ncpu32k_ipdu.v
@@ -19,11 +19,12 @@ module ncpu32k_ipdu(
    input                   clk,
    input                   rst_n,
    input [`NCPU_IW-1:0]    ipdu_insn,
-   input                   msr_psr_cc,
+   input                   bcc,
    output                  jmprel_taken,
    output [`NCPU_AW-3:0]   jmprel_offset,
    output                  jmprel_link,
-   output                  op_jmprel
+   output                  op_jmprel,
+   output                  op_jmpfar
 );
    wire [5:0] f_opcode = ipdu_insn[5:0];
    wire [25:0] f_rel26 = ipdu_insn[31:6];
@@ -32,12 +33,13 @@ module ncpu32k_ipdu(
    wire op_jmp_lnk_i = (f_opcode == `NCPU_OP_JMP_LNK_I);
    wire op_bt = (f_opcode == `NCPU_OP_BT);
    wire op_bf = (f_opcode == `NCPU_OP_BF);
+   assign op_jmpfar = (f_opcode == `NCPU_OP_JMP);
    
    // PC-Relative address (sign-extended)
    wire [`NCPU_AW-3:0] rel26 = {{`NCPU_AW-28{f_rel26[25]}}, f_rel26[25:0]};
    // PC-Relative jump
-   wire bcc = (op_bt | op_bf) & (msr_psr_cc);
-   assign jmprel_taken = (op_jmp_i | op_jmp_lnk_i) | bcc;
+   wire jmprel = (op_bt | op_bf) & (bcc);
+   assign jmprel_taken = (op_jmp_i | op_jmp_lnk_i) | jmprel;
    assign jmprel_offset = rel26;
    assign jmprel_link = op_jmp_lnk_i;
    assign op_jmprel = op_jmp_i | op_jmp_lnk_i | (op_bt | op_bf);
