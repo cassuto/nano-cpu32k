20031b92eb31c384108cb1bdcc4dbf7b1dbf787d
cassuto <diyer175@hotmail.com>
2020-03-19 21:50:48 +0800

    Updating SoPC testbench.

---
+++ testbench/tb_sopc.v
@@ -0,0 +1,89 @@
+
+`include "ncpu32k_config.h"
+
+module tb_sopc();
+
+   reg clk = 0;
+   reg sdr_clk = 0;
+   reg rst_n = 0;
+   reg DRAM_CLK = 0;
+   
+   // DRAM clk 100MHz
+   localparam tCK = 10; // ns
+   initial begin
+      // phrase +0.8ns (+28.8deg)
+      #0.8 forever #(tCK/2) sdr_clk = ~sdr_clk;
+   end
+   initial begin
+      forever #(tCK/2) DRAM_CLK = ~DRAM_CLK;
+   end
+   // Cache clk 80MHz
+   initial begin
+      forever #(12.5/2) clk = ~clk;
+   end
+
+   // Generate reset
+   initial begin
+      #10 rst_n= 1'b1;
+   end
+   
+   localparam ADDR_BITS = 13;
+   localparam BA_BITS = 2;
+   localparam DQ_BITS = 16;
+   localparam DM_BITS = 2;
+   
+   wire                     DRAM_CKE;                           // Synchronous Clock Enable
+   wire [ADDR_BITS - 1 : 0] DRAM_ADDR;                          // SDRAM Address
+   wire   [BA_BITS - 1 : 0] DRAM_BA;                            // Bank Address
+   wire   [DQ_BITS - 1 : 0] DRAM_DATA;                            // SDRAM I/O
+   wire   [DM_BITS - 1 : 0] DRAM_DQM;                           // Data Mask
+   wire                     DRAM_CAS_L;
+   wire                     DRAM_RAS_L;
+   wire                     DRAM_WE_L;
+   wire                     DRAM_CS_L;
+   
+   wire                     SPI_SCK;
+   wire                     SPI_CS_L;
+   wire                     SPI_MOSI;
+   wire                     SPI_MISO;
+   
+   // SDRAM
+   sdr
+   #(
+      .MEMH_FILE_B0 ("insn_b0.mem"),
+      .MEMH_FILE_B1 ("insn_b1.mem"),
+      .MEMH_FILE_B2 ("insn_b2.mem"),
+      .MEMH_FILE_B3 ("insn_b3.mem")
+   )
+      sdram0
+   (
+      DRAM_DATA, DRAM_ADDR, DRAM_BA,
+      DRAM_CLK, DRAM_CKE,
+      DRAM_CS_L, DRAM_RAS_L, DRAM_CAS_L, DRAM_WE_L,
+      DRAM_DQM
+   );
+
+   // SoC
+   soc_toplevel soc
+   (
+      .CPU_CLK (clk),
+      .SDR_CLK (sdr_clk),
+      .RST_L   (rst_n),
+
+      .DRAM_CKE   (DRAM_CKE),   // Synchronous Clock Enable
+      .DRAM_ADDR  (DRAM_ADDR),  // SDRAM Address
+      .DRAM_BA    (DRAM_BA),    // Bank Address
+      .DRAM_DATA  (DRAM_DATA),  // SDRAM I/O
+      .DRAM_DQM   (DRAM_DQM),   // Data Mask
+      .DRAM_CAS_L (DRAM_CAS_L),
+      .DRAM_RAS_L (DRAM_RAS_L),
+      .DRAM_WE_L  (DRAM_WE_L),
+      .DRAM_CS_L  (DRAM_CS_L),
+
+      .SPI_SCK    (SPI_SCK),
+      .SPI_CS_L   (SPI_CS_L),
+      .SPI_MOSI   (SPI_MOSI),
+      .SPI_MISO   (SPI_MISO)
+   );
+   
+endmodule
