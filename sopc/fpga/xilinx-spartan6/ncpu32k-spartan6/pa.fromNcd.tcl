dade682ebed0c5f13d89871c47d4d2bdeaed6545
cassuto <diyer175@hotmail.com>
2020-06-24 23:32:04 +0800

    Remove redundancy IP core.

--- sopc/fpga/xilinx-spartan6/ncpu32k-spartan6/pa.fromNcd.tcl
+++ sopc/fpga/xilinx-spartan6/ncpu32k-spartan6/pa.fromNcd.tcl
@@ -1,13 +1,11 @@
 
 # PlanAhead Launch Script for Post PAR Floorplanning, created by Project Navigator
 
-create_project -name ncpu32k-spartan6 -dir "E:/_processor_cores/nano-cpu32k/sopc/fpga/xilinx-spartan6/ncpu32k-spartan6/planAhead_run_5" -part xc6slx16ftg256-2
+create_project -name ncpu32k-spartan6 -dir "E:/_processor_cores/nano-cpu32k/sopc/fpga/xilinx-spartan6/ncpu32k-spartan6/planAhead_run_2" -part xc6slx16ftg256-2
 set srcset [get_property srcset [current_run -impl]]
 set_property design_mode GateLvl $srcset
-set_property edif_top_file "E:/_processor_cores/nano-cpu32k/sopc/fpga/xilinx-spartan6/ncpu32k-spartan6/toplevel.ngc" [ get_property srcset [ current_run ] ]
+set_property edif_top_file "E:/_processor_cores/nano-cpu32k/sopc/fpga/xilinx-spartan6/ncpu32k-spartan6/toplevel_cs.ngc" [ get_property srcset [ current_run ] ]
 add_files -norecurse { {E:/_processor_cores/nano-cpu32k/sopc/fpga/xilinx-spartan6/ncpu32k-spartan6} {ipcore_dir} }
-add_files [list {ipcore_dir/queue16_blk.ncf}] -fileset [get_property constrset [current_run]]
-add_files [list {ipcore_dir/queue16_fwft_blk.ncf}] -fileset [get_property constrset [current_run]]
 add_files [list {ipcore_dir/ramblk_bootrom.ncf}] -fileset [get_property constrset [current_run]]
 add_files [list {ipcore_dir/ramblk_cache_mem.ncf}] -fileset [get_property constrset [current_run]]
 set_property target_constrs_file "ncpu32k-spartan6.ucf" [current_fileset -constrset]
