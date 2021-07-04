648f0aa6630c7d94d1d6e31b15ad1fcb1da3271d
tracer-oscpu2021 <tracer@oscpu.org>
2021-07-04 10:50:51 +0800

    张三 202100001    Static hostname: HONGZHIBO-Acer          Icon name: computer-container            Chassis: container         Machine ID: d5fa2a64a3bf4270866da1f64420b3ee            Boot ID: f26c13b985b748fda1cf0f9309cf6da2     Virtualization: wsl   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.10.16.3-microsoft-standard-WSL2       Architecture: x86-64  10:50:51 up  1:09,  1 user,  load average: 0.08, 0.02, 0.01

---
+++ cpu/build/Vtop__Trace.cpp
@@ -0,0 +1,41 @@
+// Verilated -*- C++ -*-
+// DESCRIPTION: Verilator output: Tracing implementation internals
+#include "verilated_vcd_c.h"
+#include "Vtop__Syms.h"
+
+
+void Vtop__traceChgSub0(Vtop* vlSelf, VerilatedVcd* tracep);
+
+void Vtop__traceChgTop0(void* voidSelf, VerilatedVcd* tracep) {
+    Vtop* const __restrict vlSelf = static_cast<Vtop*>(voidSelf);
+    Vtop__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    if (VL_UNLIKELY(!vlSymsp->__Vm_activity)) return;
+    // Body
+    {
+        Vtop__traceChgSub0(vlSymsp->TOPp, tracep);
+    }
+}
+
+void Vtop__traceChgSub0(Vtop* vlSelf, VerilatedVcd* tracep) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vtop__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    vluint32_t* const oldp = tracep->oldp(vlSymsp->__Vm_baseCode + 1);
+    if (false && oldp) {}  // Prevent unused
+    // Body
+    {
+        tracep->chgBit(oldp+0,(vlSelf->clk));
+        tracep->chgBit(oldp+1,(vlSelf->reset));
+        tracep->chgCData(oldp+2,(vlSelf->out),4);
+    }
+}
+
+void Vtop__traceCleanup(void* voidSelf, VerilatedVcd* /*unused*/) {
+    VlUnpacked<CData/*0:0*/, 1> __Vm_traceActivity;
+    Vtop* const __restrict vlSelf = static_cast<Vtop*>(voidSelf);
+    Vtop__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    // Body
+    {
+        vlSymsp->__Vm_activity = false;
+        __Vm_traceActivity[0U] = 0U;
+    }
+}
