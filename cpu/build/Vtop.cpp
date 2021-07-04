4857faa3e0265db8418de49066bb28c4ff7e31be
tracer-oscpu2021 <tracer@oscpu.org>
2021-07-04 10:36:51 +0800

    张三 202100001    Static hostname: HONGZHIBO-Acer          Icon name: computer-container            Chassis: container         Machine ID: d5fa2a64a3bf4270866da1f64420b3ee            Boot ID: f26c13b985b748fda1cf0f9309cf6da2     Virtualization: wsl   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.10.16.3-microsoft-standard-WSL2       Architecture: x86-64  10:36:51 up 55 min,  1 user,  load average: 0.01, 0.02, 0.00

---
+++ cpu/build/Vtop.cpp
@@ -0,0 +1,139 @@
+// Verilated -*- C++ -*-
+// DESCRIPTION: Verilator output: Design implementation internals
+// See Vtop.h for the primary calling header
+
+#include "Vtop.h"
+#include "Vtop__Syms.h"
+
+//==========
+
+VerilatedContext* Vtop::contextp() const {
+    return vlSymsp->_vm_contextp__;
+}
+
+void Vtop___eval_initial(Vtop* vlSelf);
+void Vtop___eval_settle(Vtop* vlSelf);
+void Vtop___eval(Vtop* vlSelf);
+QData Vtop___change_request(Vtop* vlSelf);
+#ifdef VL_DEBUG
+void Vtop___eval_debug_assertions(Vtop* vlSelf);
+#endif  // VL_DEBUG
+void Vtop___final(Vtop* vlSelf);
+
+static void _eval_initial_loop(Vtop* vlSelf) {
+    Vtop__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    vlSymsp->__Vm_didInit = true;
+    Vtop___eval_initial(vlSelf);
+    // Evaluate till stable
+    int __VclockLoop = 0;
+    QData __Vchange = 1;
+    vlSymsp->__Vm_activity = true;
+    do {
+        VL_DEBUG_IF(VL_DBG_MSGF("+ Initial loop\n"););
+        Vtop___eval_settle(vlSelf);
+        Vtop___eval(vlSelf);
+        if (VL_UNLIKELY(++__VclockLoop > 100)) {
+            // About to fail, so enable debug to see what's not settling.
+            // Note you must run make with OPT=-DVL_DEBUG for debug prints.
+            int __Vsaved_debug = Verilated::debug();
+            Verilated::debug(1);
+            __Vchange = Vtop___change_request(vlSelf);
+            Verilated::debug(__Vsaved_debug);
+            VL_FATAL_MT("top.v", 2, "",
+                "Verilated model didn't DC converge\n"
+                "- See https://verilator.org/warn/DIDNOTCONVERGE");
+        } else {
+            __Vchange = Vtop___change_request(vlSelf);
+        }
+    } while (VL_UNLIKELY(__Vchange));
+}
+
+void Vtop::eval_step() {
+    VL_DEBUG_IF(VL_DBG_MSGF("+++++TOP Evaluate Vtop::eval_step\n"); );
+#ifdef VL_DEBUG
+    // Debug assertions
+    Vtop___eval_debug_assertions(this);
+#endif  // VL_DEBUG
+    // Initialize
+    if (VL_UNLIKELY(!vlSymsp->__Vm_didInit)) _eval_initial_loop(this);
+    // Evaluate till stable
+    int __VclockLoop = 0;
+    QData __Vchange = 1;
+    vlSymsp->__Vm_activity = true;
+    do {
+        VL_DEBUG_IF(VL_DBG_MSGF("+ Clock loop\n"););
+        Vtop___eval(this);
+        if (VL_UNLIKELY(++__VclockLoop > 100)) {
+            // About to fail, so enable debug to see what's not settling.
+            // Note you must run make with OPT=-DVL_DEBUG for debug prints.
+            int __Vsaved_debug = Verilated::debug();
+            Verilated::debug(1);
+            __Vchange = Vtop___change_request(this);
+            Verilated::debug(__Vsaved_debug);
+            VL_FATAL_MT("top.v", 2, "",
+                "Verilated model didn't converge\n"
+                "- See https://verilator.org/warn/DIDNOTCONVERGE");
+        } else {
+            __Vchange = Vtop___change_request(this);
+        }
+    } while (VL_UNLIKELY(__Vchange));
+}
+
+void Vtop::final() {
+    Vtop___final(this);
+}
+
+VL_INLINE_OPT void Vtop___sequent__TOP__1(Vtop* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vtop__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vtop___sequent__TOP__1\n"); );
+    // Body
+    vlSelf->out = ((IData)(vlSelf->reset) ? 0U : (0xfU 
+                                                  & ((IData)(1U) 
+                                                     + (IData)(vlSelf->out))));
+}
+
+void Vtop___eval(Vtop* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vtop__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vtop___eval\n"); );
+    // Body
+    if (((IData)(vlSelf->clk) & (~ (IData)(vlSelf->__Vclklast__TOP__clk)))) {
+        Vtop___sequent__TOP__1(vlSelf);
+    }
+    // Final
+    vlSelf->__Vclklast__TOP__clk = vlSelf->clk;
+}
+
+QData Vtop___change_request_1(Vtop* vlSelf);
+
+VL_INLINE_OPT QData Vtop___change_request(Vtop* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vtop__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vtop___change_request\n"); );
+    // Body
+    return (Vtop___change_request_1(vlSelf));
+}
+
+VL_INLINE_OPT QData Vtop___change_request_1(Vtop* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vtop__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vtop___change_request_1\n"); );
+    // Body
+    // Change detection
+    QData __req = false;  // Logically a bool
+    return __req;
+}
+
+#ifdef VL_DEBUG
+void Vtop___eval_debug_assertions(Vtop* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vtop__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vtop___eval_debug_assertions\n"); );
+    // Body
+    if (VL_UNLIKELY((vlSelf->clk & 0xfeU))) {
+        Verilated::overWidthError("clk");}
+    if (VL_UNLIKELY((vlSelf->reset & 0xfeU))) {
+        Verilated::overWidthError("reset");}
+}
+#endif  // VL_DEBUG
