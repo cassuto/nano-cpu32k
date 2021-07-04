3510e41fdedcd72ff7691dd9249a4753a3fe63f0
Leon <541959102@qq.com>
2021-07-04 11:17:58 +0800

    Modify directory structure

---
+++ cpu/top.v
@@ -0,0 +1,16 @@
+//top.v
+module top (
+    input clk,
+    input reset,
+    output reg [3:0] out);
+    
+    always @(posedge clk) begin
+        if(reset) begin
+            out <= 4'b0;
+        end
+        else begin
+            out <= out + 4'b1;
+        end
+    end
+    
+endmodule
