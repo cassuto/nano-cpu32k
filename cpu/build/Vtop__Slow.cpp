f783e893db51b6d2a19aaf21cb7a808c7dd32320
tracer-oscpu2021 <tracer@oscpu.org>
2021-07-04 11:08:13 +0800

    张三 202100001    Static hostname: HONGZHIBO-Acer          Icon name: computer-container            Chassis: container         Machine ID: d5fa2a64a3bf4270866da1f64420b3ee            Boot ID: f26c13b985b748fda1cf0f9309cf6da2     Virtualization: wsl   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.10.16.3-microsoft-standard-WSL2       Architecture: x86-64  11:08:13 up  1:26,  1 user,  load average: 0.00, 0.00, 0.00

---
+++ cpu/build/Vtop__Slow.cpp
@@ -0,0 +1,59 @@
+// Verilated -*- C++ -*-
+// DESCRIPTION: Verilator output: Design implementation internals
+// See Vtop.h for the primary calling header
+
+#include "Vtop.h"
+#include "Vtop__Syms.h"
+
+//==========
+
+
+void Vtop___ctor_var_reset(Vtop* vlSelf);
+
+Vtop::Vtop(VerilatedContext* _vcontextp__, const char* _vcname__)
+    : VerilatedModule{_vcname__}
+ {
+    // Create Sym instance
+    Vtop__Syms* __restrict vlSymsp = new Vtop__Syms(_vcontextp__, this, name());
+    // Reset structure values
+    Vtop___ctor_var_reset(this);
+}
+
+void Vtop::__Vconfigure(Vtop__Syms* _vlSymsp, bool first) {
+    if (false && first) {}  // Prevent unused
+    this->vlSymsp = _vlSymsp;
+}
+
+Vtop::~Vtop() {
+    VL_DO_CLEAR(delete vlSymsp, vlSymsp = nullptr);
+}
+
+void Vtop___eval_initial(Vtop* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vtop__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vtop___eval_initial\n"); );
+    // Body
+    vlSelf->__Vclklast__TOP__clk = vlSelf->clk;
+}
+
+void Vtop___eval_settle(Vtop* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vtop__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vtop___eval_settle\n"); );
+}
+
+void Vtop___final(Vtop* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vtop__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vtop___final\n"); );
+}
+
+void Vtop___ctor_var_reset(Vtop* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vtop__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vtop___ctor_var_reset\n"); );
+    // Body
+    vlSelf->clk = VL_RAND_RESET_I(1);
+    vlSelf->reset = VL_RAND_RESET_I(1);
+    vlSelf->out = VL_RAND_RESET_I(4);
+}
