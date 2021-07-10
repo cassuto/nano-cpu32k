927d68f3fdc31cb298a11cd3a826bfa77b5ab791
cassuto <diyer175@hotmail.com>
2021-07-10 20:19:10 +0800

    Update DRAM and IRAM.

---
+++ cpu/iram.v
@@ -0,0 +1,21 @@
+module iram #(
+   parameter IRAM_AW
+)
+(
+   input clk,
+   input rst,
+   output reg [31:0] o_insn,
+   input [IRAM_AW-1:0] i_addr
+);
+
+   reg [31:0] imem[1<<IRAM_AW];
+   
+   always @(posedge clk)
+      begin
+         if (rst)
+            o_insn <= 'b0;
+         else
+            o_insn <= imem[i_addr];
+      end
+
+endmodule
---
+++ cpu/mycpu.v
@@ -0,0 +1,10 @@
+// GaoZiBo
+
+module mycpu(
+   input clk,
+   input rst_n
+);
+
+   
+
+endmodule
