d0274b87b6057f9840c8b58937a8c7b7ad87cccf
tracer-oscpu2021 <tracer@oscpu.org>
2021-07-10 21:40:34 -0400

    Static hostname: alex          Icon name: computer-vm            Chassis: vm         Machine ID: a49e87052a0f48bb86e25725dfaef1d8            Boot ID: f772977ef72d4ed8897c83273d5627b2     Virtualization: oracle   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-59-generic       Architecture: x86-64  21:40:34 up 2 days, 23:50,  1 user,  load average: 2.18, 5.41, 2.67

--- cpu/id_stage.v
+++ cpu/id_stage.v
@@ -38,7 +38,7 @@ assign func3  = inst[14 : 12];
 assign rs1    = inst[19 : 15];
 assign imm    = inst[31 : 20];
 
-wire inst_addi =   ~opcode[2] & ~opcode[3] & opcode[4] & ~opcode[5] & ~opcode[5]
+wire inst_addi =   ~opcode[2] & ~opcode[3] & opcode[4] & ~opcode[5] & ~opcode[6]
                  & ~func3[0] & ~func3[1] & ~func3[2];
 
 // arith inst: 10000; logic: 01000;
