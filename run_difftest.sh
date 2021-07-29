628e8dbaecd49cf09509a63a906f40bc88942f24
tracer-oscpu2021 <tracer@oscpu.org>
2021-07-30 00:16:45 +0800

    GaoZiBo 20210479    Static hostname: ain-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 8dbdf6241b034a2daea2fb42390ead7d            Boot ID: 0cc82b7eb03441259b43f4366c7a79be     Virtualization: vmware   Operating System: Ubuntu 18.04.5 LTS             Kernel: Linux 5.4.0-80-generic       Architecture: x86-64  00:16:45 up 1 day,  3:22,  1 user,  load average: 0.42, 0.32, 0.22

--- run_difftest.sh
+++ run_difftest.sh
@@ -8,7 +8,41 @@ if [ $? -ne 0 ]; then
    exit 1
 fi
 
-for fn in `ls /mnt/oscpu/am-kernels/tests/cpu-tests/build/*.bin`; do
+list = "/mnt/oscpu/am-kernels/tests/cpu-tests/build/add-longlong-riscv64-mycpu.bin
+/mnt/oscpu/am-kernels/tests/cpu-tests/build/add-riscv64-mycpu.bin
+/mnt/oscpu/am-kernels/tests/cpu-tests/build/bit-riscv64-mycpu.bin
+/mnt/oscpu/am-kernels/tests/cpu-tests/build/bubble-sort-riscv64-mycpu.bin
+/mnt/oscpu/am-kernels/tests/cpu-tests/build/div-riscv64-mycpu.bin
+/mnt/oscpu/am-kernels/tests/cpu-tests/build/dummy-riscv64-mycpu.bin
+/mnt/oscpu/am-kernels/tests/cpu-tests/build/fact-riscv64-mycpu.bin
+/mnt/oscpu/am-kernels/tests/cpu-tests/build/fib-riscv64-mycpu.bin
+/mnt/oscpu/am-kernels/tests/cpu-tests/build/goldbach-riscv64-mycpu.bin
+/mnt/oscpu/am-kernels/tests/cpu-tests/build/if-else-riscv64-mycpu.bin
+/mnt/oscpu/am-kernels/tests/cpu-tests/build/leap-year-riscv64-mycpu.bin
+/mnt/oscpu/am-kernels/tests/cpu-tests/build/load-store-riscv64-mycpu.bin
+/mnt/oscpu/am-kernels/tests/cpu-tests/build/matrix-mul-riscv64-mycpu.bin
+/mnt/oscpu/am-kernels/tests/cpu-tests/build/max-riscv64-mycpu.bin
+/mnt/oscpu/am-kernels/tests/cpu-tests/build/min3-riscv64-mycpu.bin
+/mnt/oscpu/am-kernels/tests/cpu-tests/build/mov-c-riscv64-mycpu.bin
+/mnt/oscpu/am-kernels/tests/cpu-tests/build/movsx-riscv64-mycpu.bin
+/mnt/oscpu/am-kernels/tests/cpu-tests/build/mul-longlong-riscv64-mycpu.bin
+/mnt/oscpu/am-kernels/tests/cpu-tests/build/pascal-riscv64-mycpu.bin
+/mnt/oscpu/am-kernels/tests/cpu-tests/build/prime-riscv64-mycpu.bin
+/mnt/oscpu/am-kernels/tests/cpu-tests/build/quick-sort-riscv64-mycpu.bin
+/mnt/oscpu/am-kernels/tests/cpu-tests/build/recursion-riscv64-mycpu.bin
+/mnt/oscpu/am-kernels/tests/cpu-tests/build/select-sort-riscv64-mycpu.bin
+/mnt/oscpu/am-kernels/tests/cpu-tests/build/shift-riscv64-mycpu.bin
+/mnt/oscpu/am-kernels/tests/cpu-tests/build/shuixianhua-riscv64-mycpu.bin
+/mnt/oscpu/am-kernels/tests/cpu-tests/build/string-riscv64-mycpu.bin
+/mnt/oscpu/am-kernels/tests/cpu-tests/build/sub-longlong-riscv64-mycpu.bin
+/mnt/oscpu/am-kernels/tests/cpu-tests/build/sum-riscv64-mycpu.bin
+/mnt/oscpu/am-kernels/tests/cpu-tests/build/switch-riscv64-mycpu.bin
+/mnt/oscpu/am-kernels/tests/cpu-tests/build/to-lower-case-riscv64-mycpu.bin
+/mnt/oscpu/am-kernels/tests/cpu-tests/build/unalign-riscv64-mycpu.bin
+/mnt/oscpu/am-kernels/tests/cpu-tests/build/wanshu-riscv64-mycpu.bin"
+# `ls /mnt/oscpu/am-kernels/tests/cpu-tests/build/*.bin`
+
+for fn in $list; do
    echo ============================
    echo Running $fn
    echo ============================
