30c6b83b5301462f53d14585b0078d21c7559785
tracer-oscpu2021 <tracer@oscpu.org>
2021-07-29 23:33:28 +0800

    GaoZiBo 20210479    Static hostname: ain-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 8dbdf6241b034a2daea2fb42390ead7d            Boot ID: 0cc82b7eb03441259b43f4366c7a79be     Virtualization: vmware   Operating System: Ubuntu 18.04.5 LTS             Kernel: Linux 5.4.0-80-generic       Architecture: x86-64  23:33:28 up 1 day,  2:39,  1 user,  load average: 0.11, 0.14, 0.29

--- run_difftest.sh
+++ run_difftest.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 #./build.sh -e cpu_diff -d -b -s -a "-i ../../../testcase/addi.bin --dump-wave" -m "EMU_TRACE=1" -w
+./build.sh -e cpu_diff -d -b -s -a "-i /mnt/oscpu/am-kernels/tests/cpu-tests/build/bit-riscv64-mycpu.bin --dump-wave" -m "EMU_TRACE=1" -w
+exit 0
 
 ./build.sh -e cpu_diff -d -b
 if [ $? -ne 0 ]; then
@@ -10,7 +12,7 @@ for fn in `ls /mnt/oscpu/am-kernels/tests/cpu-tests/build/*.bin`; do
    echo ============================
    echo Running $fn
    echo ============================
-   ./build.sh -e cpu_diff -d -s -a "-i $fn --dump-wave" -m "EMU_TRACE=1" -m "VM_TRACE=1"
+   ./build.sh -e cpu_diff -d -s -a "-i $fn --dump-wave" -m "EMU_TRACE=1"
    if [ $? -ne 0 ]; then
       exit 1
    fi
