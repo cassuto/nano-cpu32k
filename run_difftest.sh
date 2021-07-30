aec243bb5479441f5addf65cfa196060ad66195d
tracer-oscpu2021 <tracer@oscpu.org>
2021-07-30 18:32:23 +0800

    GaoZiBo 20210479    Static hostname: ain-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 8dbdf6241b034a2daea2fb42390ead7d            Boot ID: 0cc82b7eb03441259b43f4366c7a79be     Virtualization: vmware   Operating System: Ubuntu 18.04.5 LTS             Kernel: Linux 5.4.0-80-generic       Architecture: x86-64  18:32:23 up 1 day,  6:16,  1 user,  load average: 0.24, 0.22, 0.18

--- run_difftest.sh
+++ run_difftest.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-#./build.sh -e cpu_diff -d -b -s -a "-i ../../../testcase/addi.bin --dump-wave" -m "EMU_TRACE=1" -w
-./build.sh -e cpu_diff -d -b -s -a "-i /mnt/oscpu/am-kernels/tests/cpu-tests/build/hello-str-riscv64-mycpu.bin --dump-wave" -m "EMU_TRACE=1" -w
+./build.sh -e cpu_diff -d -b -s -a "-i ../../../testcase/addi.bin --dump-wave" -m "EMU_TRACE=1" -w
+#./build.sh -e cpu_diff -d -b -s -a "-i /mnt/oscpu/am-kernels/tests/cpu-tests/build/hello-str-riscv64-mycpu.bin --dump-wave" -m "EMU_TRACE=1" -w
 exit 0
 
 #./build.sh -e cpu_diff -d -b
