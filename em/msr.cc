96dc2726d1e2138a5a97fefcba4ada4fddc74df4
tracer-oscpu2021 <tracer@oscpu.org>
2021-08-25 15:18:24 +0800

    GaoZiBo 20210479    Static hostname: ain-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 8dbdf6241b034a2daea2fb42390ead7d            Boot ID: 0cc82b7eb03441259b43f4366c7a79be     Virtualization: vmware   Operating System: Ubuntu 18.04.5 LTS             Kernel: Linux 5.4.0-80-generic       Architecture: x86-64 Wed Aug 25 15:18:24 CST 2021

--- em/msr.cc
+++ em/msr.cc
@@ -138,11 +138,11 @@ void CPU::wmsr(msr_index_t index, cpu_word_t v)
             break;
         /* MSR bank - DCA */
         case MSR_DCINV:
-        printf("d INV paddr=%#x\n", val);
+        //printf("d INV paddr=%#x\n", val);
             dcache->invalidate(val);
             break;
         case MSR_DCFLS:
-        printf("d FLS paddr=%#x\n", val);
+        //printf("d FLS paddr=%#x\n", val);
             dcache->flush(val);
             break;
 
