4485d9b6c5802b5c60e8cb069fe9210f4a40f0cd
cassuto <diyer175@hotmail.com>
2021-08-26 18:28:23 +0800

    Add fetch buffer

---
+++ rtl/fifo_fwft.v
@@ -0,0 +1,96 @@
+/*
+Copyright 2021 GaoZiBo <diyer175@hotmail.com>
+Powered by YSYX https://oscpu.github.io/ysyx
+
+Licensed under The MIT License (MIT).
+-------------------------------------
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED,INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+module fifo_fwft
+#(
+   parameter DW = 8, // Data Width in bits
+   parameter DEPTH_WIDTH = 4 // Width of depth
+)
+(
+   input                               clk,
+   input                               rst,
+   input                               flush,
+   input                               push,
+   input [DW-1:0]                      din,
+   output                              ready,
+   input                               pop,
+   output [DW-1:0]                     dout,
+   output                              valid
+);
+   wire [DEPTH_WIDTH:0]                ff_w_ptr;
+   wire [DEPTH_WIDTH:0]                w_ptr_nxt;
+   wire [DEPTH_WIDTH:0]                ff_r_ptr;
+   wire [DEPTH_WIDTH:0]                r_ptr_nxt;
+   wire [DW-1:0]                       rf_dout, rf_dout_byp;
+   wire                                state_r;
+   wire                                fwft_nxt;
+   wire                                clr_state;
+   wire [DW-1:0]                       dat_r, din_r;
+   wire                                rf_conflict;
+   wire                                rf_bypass;
+
+   assign w_ptr_nxt = (ff_w_ptr + 1'd1) & {DEPTH_WIDTH+1{~flush}};
+   assign r_ptr_nxt = (ff_r_ptr + 1'd1) & {DEPTH_WIDTH+1{~flush}};
+
+   mDFF_lr #(.DW(DEPTH_WIDTH + 1)) ff_w_ptr_r (.CLK(clk), .RST(rst), .LOAD(push|flush), .D(w_ptr_nxt), .Q(ff_w_ptr) );
+   mDFF_lr #(.DW(DEPTH_WIDTH + 1)) ff_r_ptr_r (.CLK(clk), .RST(rst), .LOAD(pop|flush), .D(r_ptr_nxt), .Q(ff_r_ptr) );
+
+   assign ready = (ff_w_ptr[DEPTH_WIDTH] == ff_r_ptr[DEPTH_WIDTH]) |
+                  (ff_w_ptr[DEPTH_WIDTH-1:0] != ff_r_ptr[DEPTH_WIDTH-1:0]); // Not full
+   assign valid = (ff_w_ptr != ff_r_ptr); // Not empty
+
+   assign fwft_nxt = (~state_r & valid & push);
+   assign clr_state = (state_r & pop);
+
+   // FWFT FSM
+   mDFF_lr #(.DW(DW)) ff_dat (.CLK(clk), .RST(rst), .LOAD(fwft_nxt), .D(din), .Q(dat_r) );
+   mDFF_lr #(.DW(DW)) ff_din (.CLK(clk), .RST(rst), .LOAD(pop), .D(din), .Q(din_r) );
+   mDFF_lr #(.DW(1)) ff_state (.CLK(clk),.RST(rst), .LOAD(fwft_nxt|clr_state|flush), .D((fwft_nxt|~clr_state) & ~flush), .Q(state_r) );
+
+   assign dout = state_r ? dat_r : rf_dout_byp;
+   
+   mRF_1w1r
+      #(
+         .DW (DW),
+         .AW (DEPTH_WIDTH)
+      )
+   U_RF
+      (
+         .CLK     (clk),
+         .RE      (pop),
+         .RADDR   (r_ptr_nxt[DEPTH_WIDTH-1:0]),
+         .RDATA   (rf_dout),
+         .WE      (push),
+         .WADDR   (ff_w_ptr[DEPTH_WIDTH-1:0]),
+         .WDATA   (din)
+      );
+      
+   assign rf_conflict = ((ff_w_ptr[DEPTH_WIDTH-1:0] == r_ptr_nxt[DEPTH_WIDTH-1:0]) & push & pop);
+
+   // Bypass FSM
+   mDFF_lr #(.DW(1)) ff_bypass (.CLK(clk), .RST(rst), .LOAD(rf_conflict | pop), .D(rf_conflict | ~pop), .Q(rf_bypass) );
+
+   assign rf_dout_byp = rf_bypass ? din_r : rf_dout;
+
+endmodule
---
+++ rtl/iq.v
@@ -0,0 +1,170 @@
+/*
+Copyright 2021 GaoZiBo <diyer175@hotmail.com>
+Powered by YSYX https://oscpu.github.io/ysyx
+
+Licensed under The MIT License (MIT).
+-------------------------------------
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED,INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+`include "ncpu64k_config.vh"
+
+module iq
+#(
+   parameter CONFIG_AW = 32,
+   parameter CONFIG_P_FETCH_WIDTH = 2,
+   parameter CONFIG_P_ISSUE_WIDTH = 2,
+   parameter CONFIG_P_IQ_DEPTH = 4 // Depth per fetch channel
+)
+(
+   input                               clk,
+   input                               rst,
+   input                               flush,
+   input [`NCPU_INSN_DW * (1<<CONFIG_P_FETCH_WIDTH)-1:0] iq_ins,
+   input [CONFIG_AW * (1<<CONFIG_P_FETCH_WIDTH)-1:0] iq_pc,
+   input [`FNT_EXC_W * (1<<CONFIG_P_FETCH_WIDTH)-1:0] iq_exc,
+   input [CONFIG_P_FETCH_WIDTH:0]      iq_push_cnt,
+   output                              iq_ready,
+   // To ID
+   output [(1<<CONFIG_P_ISSUE_WIDTH)-1:0] id_valid,
+   input [CONFIG_P_ISSUE_WIDTH:0]      id_pop_cnt,
+   output [`NCPU_INSN_DW * (1<<CONFIG_P_ISSUE_WIDTH)-1:0] id_ins,
+   output [CONFIG_AW * (1<<CONFIG_P_ISSUE_WIDTH)-1:0] id_pc,
+   output [`FNT_EXC_W * (1<<CONFIG_P_ISSUE_WIDTH)-1:0] id_exc
+);
+   localparam FW                       = (1<<CONFIG_P_FETCH_WIDTH);
+   localparam IW                       = (1<<CONFIG_P_ISSUE_WIDTH);
+   localparam FIFO_DW                  = (`NCPU_INSN_DW + CONFIG_AW + `FNT_EXC_W); // INST + PC + EXC
+   localparam P_BANKS                  = (CONFIG_P_FETCH_WIDTH);
+   localparam BANKS                    = (1<<P_BANKS);
+
+   wire [P_BANKS-1:0]                  head_ff, tail_ff;
+   wire [P_BANKS-1:0]                  head_nxt, tail_nxt;
+   wire [P_BANKS-1:0]                  head_l [FW-1:0];
+   wire [P_BANKS-1:0]                  head_r [FW-1:0], tail_r [FW-1:0], tail_inv [FW-1:0];
+   wire [FIFO_DW-1:0]                  que_din [BANKS-1:0];
+   wire [FIFO_DW-1:0]                  que_dout [BANKS-1:0];
+   wire                                que_valid [BANKS-1:0];
+   wire [BANKS-1:0]                    que_ready;
+   wire                                que_push [BANKS-1:0];
+   wire                                que_pop [BANKS-1:0];
+   genvar i;
+
+   generate
+      for(i=0;i<BANKS;i=i+1)
+         begin : gen_ptr
+            assign head_l[i]  = i + head_ff;
+            assign head_r[i]  = i - head_ff;
+            assign tail_r[i] = i - tail_ff;
+            
+            //
+            // The data layout in a fetch window is as follows (FW=4)
+            // Case: push_cnt = 4:
+            // Idx   0   1   2   3
+            //     +---+---+---+---+
+            // Dat |D0 |D1 |D2 |D3 |
+            //     +---+---+---+---+
+            // Case: push_cnt = 3:
+            //     +---+---+---+---+
+            // Dat |X  |D0 |D1 |D2 |
+            //     +---+---+---+---+
+            // Case: push_cnt = 2:
+            //     +---+---+---+---+
+            // Dat |X  |X  |D0 |D1 |
+            //     +---+---+---+---+
+            // Case: push_cnt = 1:
+            //     +---+---+---+---+
+            // Dat |X  |X  |X  |D0 |
+            //     +---+---+---+---+
+            //
+            assign tail_inv[i] = i - tail_ff + (FW-iq_push_cnt);
+         end
+   endgenerate
+   
+   // MUX for FIFO input
+   generate
+      for(i=0;i<BANKS;i=i+1)
+         begin : gen_bank_ctrl
+            assign que_din[i] = {iq_ins[tail_inv[i]],
+                                 iq_pc[tail_inv[i]],
+                                 iq_exc[tail_inv[i]]};
+            assign que_pop[i]  = ({1'b0, head_r[i]} < id_pop_cnt);
+            assign que_push[i] = ({1'b0, tail_r[i]} < iq_push_cnt);
+         end
+   endgenerate
+   
+   assign head_nxt = (head_ff + id_pop_cnt[P_BANKS-1:0]) & {P_BANKS{~flush}};
+   assign tail_nxt = (tail_ff + iq_push_cnt[P_BANKS-1:0]) & {P_BANKS{~flush}};
+   
+   mDFF_r #(.DW(P_BANKS)) ff_head (.CLK(clk), .RST(rst), .D(head_nxt), .Q(head_ff) );
+   mDFF_r #(.DW(P_BANKS)) ff_tail (.CLK(clk), .RST(rst), .D(tail_nxt), .Q(tail_ff) );
+   
+   generate
+      for(i=0;i<BANKS;i=i+1)
+         begin
+            fifo_fwft
+               #(
+                  .DW            (`NCPU_INSN_DW),
+                  .DEPTH_WIDTH   (CONFIG_P_IQ_DEPTH)
+               )
+            U_FIFO
+               (
+                  .clk           (clk),
+                  .rst           (rst),
+                  .flush         (flush),
+                  .push          (que_push[i]),
+                  .din           (que_din[i]),
+                  .ready         (que_ready[i]),
+                  .pop           (que_pop[i]),
+                  .dout          (que_dout[i]),
+                  .valid         (que_valid[i])
+               );
+         end
+   endgenerate
+   
+   // MUX for data output
+   generate
+      for(i=0;i<(1<<CONFIG_P_ISSUE_WIDTH);i=i+1)
+         begin : gen_pop
+            assign {id_ins[i*`NCPU_INSN_DW +: `NCPU_INSN_DW],
+                     id_pc[i*CONFIG_AW +: CONFIG_AW],
+                     id_exc[i*`FNT_EXC_W +: `FNT_EXC_W]} = que_dout[head_l[i]];
+            assign id_valid[i] = que_valid[head_l[i]];
+         end
+   endgenerate
+   
+   assign iq_ready = &que_ready;
+   
+   
+   // synthesis translate_off
+`ifndef SYNTHESIS
+   initial
+      begin
+         if (CONFIG_P_IQ_DEPTH < CONFIG_P_FETCH_WIDTH)
+            $fatal(1, "Unreasonable configuration of IQ depth.");
+         if (CONFIG_P_ISSUE_WIDTH > CONFIG_P_FETCH_WIDTH)
+            $fatal(1, "Unreasonable configuration of fetch width or issue width.");
+      end
+      
+   always @(posedge clk)
+      if (~iq_ready & (|iq_push_cnt))
+         $fatal(1, "BUG ON: FIFO is overflow.");
+`endif
+   // synthesis translate_on
+
+endmodule
