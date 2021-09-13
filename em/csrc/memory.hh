e54557c334e7dba2b9820d8a33411e438e15debf
tracer-oscpu2021 <tracer@oscpu.org>
2021-09-13 17:57:15 +0800

    GaoZiBo 20210479    Static hostname: ain-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 8dbdf6241b034a2daea2fb42390ead7d            Boot ID: 0cc82b7eb03441259b43f4366c7a79be     Virtualization: vmware   Operating System: Ubuntu 18.04.5 LTS             Kernel: Linux 5.4.0-80-generic       Architecture: x86-64 Mon Sep 13 17:57:15 CST 2021

--- em/csrc/memory.hh
+++ em/csrc/memory.hh
@@ -47,7 +47,7 @@ public:
         }
         else if (addr >= dram_phy_start && addr < (dram_phy_start + memory_size))
         {
-          if (cpu && addr==0x8037a248){
+          if (cpu /*&& addr==0x8037a248*/){
             printf("w %#x pa=%#x\n", cpu->get_pc(), addr);
           }
             memory[addr - dram_phy_start] = val;
