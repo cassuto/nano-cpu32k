959eba00ed06d0b5c1912fd79eebe29bf2515893
tracer-oscpu2021 <tracer@oscpu.org>
2021-09-15 15:48:09 +0800

    GaoZiBo 20210479    Static hostname: ain-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 8dbdf6241b034a2daea2fb42390ead7d            Boot ID: 0cc82b7eb03441259b43f4366c7a79be     Virtualization: vmware   Operating System: Ubuntu 18.04.5 LTS             Kernel: Linux 5.4.0-80-generic       Architecture: x86-64 Wed Sep 15 15:48:09 CST 2021

--- rtl/immu.v
+++ rtl/immu.v
@@ -161,21 +161,21 @@ endgenerate
    
    // synthesis translate_off
 `ifndef SYNTHESIS
-`ifdef NCPU_ENABLE_ASSERT
+//`ifdef NCPU_ENABLE_ASSERT
 
    // Assertions
    initial
       begin
          // Assertion (03061058)
          if (!(CONFIG_ITLB_P_SETS <= VPN_DW))
-            $fatal (0, "\n CONFIG_ITLB_P_SETS should <= VPN_DW\n");
+            $fatal (1, "\n CONFIG_ITLB_P_SETS should <= VPN_DW\n");
       end
       
-   always @(posedge clk)
+   /*always @(posedge clk)
       if (EITM & EIPF)
-         $fatal ("\n EITM and EIPF should be mutex\n");
+         $fatal (1, "\n EITM and EIPF should be mutex\n");*/
 
-`endif
+//`endif
 `endif
    // synthesis translate_on
 
