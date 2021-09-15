11b1211e6753db392ad95402c23f2a58160bf0c4
tracer-oscpu2021 <tracer@oscpu.org>
2021-09-15 18:26:24 +0800

    GaoZiBo 20210479    Static hostname: ain-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 8dbdf6241b034a2daea2fb42390ead7d            Boot ID: 0cc82b7eb03441259b43f4366c7a79be     Virtualization: vmware   Operating System: Ubuntu 18.04.5 LTS             Kernel: Linux 5.4.0-80-generic       Architecture: x86-64 Wed Sep 15 18:26:24 CST 2021

--- em/csrc/third-party/dram-axi4-model.cc
+++ em/csrc/third-party/dram-axi4-model.cc
@@ -216,7 +216,7 @@ void DRAM::dramsim3_helper_rising(const axi_channel &axi)
         {
             uint64_t address = wait_resp_b->req->address % mem->get_size();
             mem->dram_writem64(address / sizeof(uint64_t) + i, meta->data[i]);
-            // printf("flush write to memory[0x%ld] = 0x%lx\n", address)
+             printf("flush write to memory[0x%ld] = 0x%lx\n", address);
         }
 #endif
         delete meta;
@@ -256,7 +256,7 @@ void DRAM::dramsim3_helper_rising(const axi_channel &axi)
 #endif
         axi_get_wdata(axi, data_start, src_addr, meta->size);
         meta->offset++;
-         printf("accept a new write data\n");
+        // printf("accept a new write data\n");
     }
     if (wait_req_w)
     {
