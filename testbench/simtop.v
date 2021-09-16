6f3f5ba1254e2d56bf7bf7585bf15302eadc9320
tracer-oscpu2021 <tracer@oscpu.org>
2021-09-16 08:44:40 +0800

    GaoZiBo 20210479    Static hostname: ain-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 8dbdf6241b034a2daea2fb42390ead7d            Boot ID: 0cc82b7eb03441259b43f4366c7a79be     Virtualization: vmware   Operating System: Ubuntu 18.04.5 LTS             Kernel: Linux 5.4.0-80-generic       Architecture: x86-64 Thu Sep 16 08:44:40 CST 2021

--- testbench/simtop.v
+++ testbench/simtop.v
@@ -283,7 +283,7 @@ module simtop
     
    assign break_point = /*(U_DUT.U_CORE.U_EX.U_LSU.D_CACHE.s2i_payload_addr == 'h398) &&
                            (|U_DUT.U_CORE.U_EX.U_LSU.D_CACHE.s2i_payload_we[2]);*/
-                           (U_DUT.U_CORE.dbus_AWADDR == 'h80007E40) &
+                           (U_DUT.U_CORE.dbus_AWADDR == 'h80FFFFC0) &
                            U_DUT.U_CORE.dbus_AWVALID;
    //assign break_point = 'b0;
     
