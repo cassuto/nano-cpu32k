624e4a1d79e51f7de2022c7a4635ed54a7d3a09d
tracer-oscpu2021 <tracer@oscpu.org>
2021-09-23 20:29:24 +0800

    GaoZiBo 20210479    Static hostname: ain-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 8dbdf6241b034a2daea2fb42390ead7d            Boot ID: 0cc82b7eb03441259b43f4366c7a79be     Virtualization: vmware   Operating System: Ubuntu 18.04.5 LTS             Kernel: Linux 5.4.0-80-generic       Architecture: x86-64 Thu Sep 23 20:29:24 CST 2021

--- em/csrc/cpu.cc
+++ em/csrc/cpu.cc
@@ -339,7 +339,7 @@ CPU::step(vm_addr_t pc, bool difftest, ArchEvent *event)
             readout = dcache->phy_readm32(pa);
         set_reg(rd, readout);
         if(pc==0x800001a8){
-            printf("restor EPC %#x\n", readout);
+            printf("restor va=%#x EPC %#x\n", va, readout);
         }
     }
     break;
@@ -368,7 +368,7 @@ CPU::step(vm_addr_t pc, bool difftest, ArchEvent *event)
         else
             dcache->phy_writem32(pa, (uint32_t)get_reg(rd));
         if(pc==0x80008dd4){
-            printf("store EPC=%#x\n", (uint32_t)get_reg(rd));
+            printf("store va=%#x EPC=%#x\n", va, (uint32_t)get_reg(rd));
         }
     }
     break;
