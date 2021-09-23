9b437bff5c984d566e265ed943f970ba78a0207f
tracer-oscpu2021 <tracer@oscpu.org>
2021-09-23 21:45:19 +0800

    GaoZiBo 20210479    Static hostname: ain-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 8dbdf6241b034a2daea2fb42390ead7d            Boot ID: 0cc82b7eb03441259b43f4366c7a79be     Virtualization: vmware   Operating System: Ubuntu 18.04.5 LTS             Kernel: Linux 5.4.0-80-generic       Architecture: x86-64 Thu Sep 23 21:45:19 CST 2021

--- em/csrc/msr.cc
+++ em/csrc/msr.cc
@@ -135,6 +135,9 @@ void CPU::wmsr(msr_index_t index, cpu_word_t v)
                 if(msr.PSR.IRE){
                     printf("enaIRQ pc=%#x cyc=%d\n", pc, cnt);
             }
+            if (cnt==944) {
+                ras->dump();
+            }
 
             ++cnt;
             }
