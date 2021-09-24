e66f4bab38efc0b6271f6cc502d83a99358e6a97
tracer-oscpu2021 <tracer@oscpu.org>
2021-09-24 13:52:56 +0800

    GaoZiBo 20210479    Static hostname: ain-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 8dbdf6241b034a2daea2fb42390ead7d            Boot ID: 0cc82b7eb03441259b43f4366c7a79be     Virtualization: vmware   Operating System: Ubuntu 18.04.5 LTS             Kernel: Linux 5.4.0-80-generic       Architecture: x86-64 Fri Sep 24 13:52:56 CST 2021

--- em/csrc/cpu.cc
+++ em/csrc/cpu.cc
@@ -610,6 +610,9 @@ void CPU::run_step()
     if(pc==0x8000019c){
         printf("rt_hw_context_switch_exit r2+12=%#x\n", get_reg(2)+12);
     }
+    if(pc==0x80008710){
+        printf("%#x r3=%#x\n", pc, get_reg(3));
+    }
     pc = npc;
 }
 
